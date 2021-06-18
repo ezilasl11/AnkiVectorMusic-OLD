d65a7f44e11ea951fd851ae8e5db6a682d58a63b
Ezila <85987071+ezilasl11@users.noreply.github.com>
2021-06-18 10:04:12 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -9,19 +9,19 @@ from config import BOT_NAME as bn
 @Client.on_message(filters.command("start") & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""Hello 👋 there! I can play music in voice chats of Telegeam Groups. I have a lot of cool feature that will amaze you!\n\n🔴 Do you want me to play music in your Telegram groups'voice chats? Please click the \'📃 User Manual 📃\' button below to know how you can use me.\n\n🔴 The Assistant must be in your group to play music in the voice chat of your group.\n\n🔴 More info & commands mentioned in the [User Manual](https://telegra.ph/Anki-Vector-Music-05-07)\n\nA project by @Damantha_Jasinghe\n Join Our Group @SLTBrecLand""",
+        f"""Hello 👋 there! I can play music in voice chats of Telegeam Groups. I have a lot of cool feature that will amaze you!\n\n🔴 Do you want me to play music in your Telegram groups'voice chats? Please click the \'📃 User Manual 📃\' button below to know how you can use me.\n\n🔴 The Assistant must be in your group to play music in the voice chat of your group.\n\n🔴 More info & commands mentioned in the [User Manual](https://telegra.ph/file/d5f0ee06089ac31379a19.jpg)\n\nA project by @Darkridersslk\n Join Our Group @joingrouplk""",
         reply_markup=InlineKeyboardMarkup(
             [ 
                 [
                     InlineKeyboardButton(
-                        "📃 User Manual 📃", url="https://telegra.ph/Anki-Vector-Music-05-07")
+                        "📃 User Manual 📃", url="https://telegra.ph/file/d5f0ee06089ac31379a19.jpg")
                   ],[
                     InlineKeyboardButton(
-                        "👨‍💻 Updates 👨‍💻", url="https://t.me/ankivectorUpdates"
+                        "👨‍💻 Updates 👨‍💻", url="https://t.me/Ezila_Updates"
                     )
                 ],[ 
                     InlineKeyboardButton(
-                        "Support Chat 🎙️", url="https://t.me/AnkiSupport_Official"
+                        "Support Chat 🎙️", url="https://t.me/Ezila_Support"
                     )]
             ]
         ),
@@ -35,7 +35,7 @@ async def gstart(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🎙️ Support Group 🎙️", url="https://t.me/AnkiSupport_Official")
+                        "🎙️ Support Group 🎙️", url="https://t.me/Ezila_Support")
                 ]
             ]
         )
